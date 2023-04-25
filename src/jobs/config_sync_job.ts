04cefb1eeff5e0e6770b001d4d44317c9b8b83fb
hejdamax <hejdamax@fit.cvut.cz>
2023-04-25 22:14:25 +0200

    Added more context for config job error logging.

--- src/jobs/config_sync_job.ts
+++ src/jobs/config_sync_job.ts
@@ -117,6 +117,8 @@ export class ConfigSyncJob extends SyncJob {
         checkedMappings.push(mapping);
       } catch (ex) {
         operationsOk = false;
+        //TODO figure out better way to work with extra context.
+        this._sentryService.createExtraContext('Object_to_sync', JSON.parse(JSON.stringify(objectToSync)));
         this._sentryService.logError(ex);
       }
     }
@@ -327,17 +329,23 @@ export class ConfigSyncJob extends SyncJob {
       newObject = await serviceWrapper.syncedService.createServiceObject(objectToSync.id, objectToSync.name, objectToSync.type);
     } catch (ex: any) {
       if (ex.status !== 400) {
-        let context = this._sentryService.createExtraContext("Status_code", ex.status);
-        this._sentryService.logError(ex, context);
         throw ex;
       }
+      // For debugging purposes catching all errors here.
+      let context = [
+        this._sentryService.createExtraContext("Status_code", ex.status),
+        this._sentryService.createExtraContext('Object_to_sync', {'id': objectToSync.id, 'name': objectToSync.name, 'type': objectToSync.type})
+      ]
+      this._sentryService.logError(ex, context);
       // 400 ~ maybe object already exists and cannot be created (for example object needs to be unique - name)?
       // => try to find it and use it for the mapping
       const serviceObjectName = serviceWrapper.syncedService.getFullNameForServiceObject(new ServiceObject(objectToSync.id, objectToSync.name, objectToSync.type));
       newObject = serviceWrapper.allServiceObjects.find(serviceObject => serviceObject.name === serviceObjectName);
       if (!newObject) {
-        this._sentryService.logError(ex);
-
+        let context = [
+          this._sentryService.createExtraContext('Object_to_sync', {'id': objectToSync.id, 'name': objectToSync.name, 'type': objectToSync.type})
+        ]
+        this._sentryService.logError(ex, context);
         throw ex;
       }
       // console.log(`ConfigSyncJob: Creating mapping, but object exists, using real object ${newObject.name}`);
@@ -363,6 +371,9 @@ export class ConfigSyncJob extends SyncJob {
           // service object is missing, it is ok to delete the mapping
           operationOk = true;
         } else {
+          let context = [
+            this._sentryService.createExtraContext('Mapping to delete', {'id': mappingObject.id, 'type': mappingObject.type})
+          ]
           this._sentryService.logError(ex);
           // console.error('err: ConfigSyncJob: delete; exception');
         }
