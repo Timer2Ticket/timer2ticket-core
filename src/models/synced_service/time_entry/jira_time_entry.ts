292458dd4fd64f86bcbbe1414320f457c1655e27
Jan Starustka <starujan@fit.cvut.cz>
2023-11-09 12:16:42 +0100

    jiraTimeEntry defined

---
+++ src/models/synced_service/time_entry/jira_time_entry.ts
@@ -0,0 +1,24 @@
+import { TimeEntry } from "./time_entry";
+
+export class JiraTimeEntry implements TimeEntry {
+    id: number | string;
+    projectId: number | string;
+    text: string;
+    start: Date | number | string;
+    end: Date | number | string;
+    durationInMilliseconds: number;
+    lastUpdated: Date | number | string;
+    issueId: number | string;
+
+    constructor(id: number | string, projectId: number | string, text: string, start: Date | number | string, end: Date | number | string,
+        duration: number, lastUpdated: Date | number | string, issueId: number | string) {
+        this.id = id;
+        this.projectId = projectId;
+        this.text = text;
+        this.start = start;
+        this.end = end;
+        this.durationInMilliseconds = duration;
+        this.issueId = issueId;
+        this.lastUpdated = lastUpdated;
+    }
+}
