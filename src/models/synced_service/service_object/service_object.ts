bc6535a50f80afbea7862e7c828863b9d73d97f0
Jan Starustka <starujan@fit.cvut.cz>
2024-02-01 09:27:53 +0100

    atributes needed for 2projectSync added

--- src/models/synced_service/service_object/service_object.ts
+++ src/models/synced_service/service_object/service_object.ts
@@ -2,10 +2,14 @@ export class ServiceObject {
   id: number | string;
   name: string;
   type: string;
+  projectId: number | string | null
+  syncCustomFieldValue: number | string | null
 
-  constructor(id: number | string, name: string, type: string) {
+  constructor(id: number | string, name: string, type: string, projectId: number | string | null = null, syncCustomFieldValue: number | string | null = null) {
     this.id = id;
     this.name = name;
     this.type = type;
+    this.projectId = projectId
+    this.syncCustomFieldValue = syncCustomFieldValue
   }
 }
