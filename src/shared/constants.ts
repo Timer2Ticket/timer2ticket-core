a12813b9c117da84bb57cd949f1bc56b24d9094c
Jan Starustka <starujan@fit.cvut.cz>
2024-02-12 13:02:07 +0100

    sentry put in operation

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -8,7 +8,7 @@ export class Constants {
 
   static mongoDbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
 
-  static sentryDsn = process.env.SENTRY_DSN || '';
+  static sentryDsn = process.env.SENTRY_DSN || 'https://d9e4e19c9f15821d57796be76000be61@o4506110897750016.ingest.sentry.io/4506110899781632';
   static daysToSync = 60;
 
   static defaultWaitDurationInCaseOfTooManyRequestsInMilliseconds = 1500;
