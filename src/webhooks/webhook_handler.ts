fb80bb514c9d12301f86812c8eed236ca4cd3c46
Jan Starustka <starujan@fit.cvut.cz>
2024-02-15 15:34:46 +0100

    create issue webhook works

--- src/webhooks/webhook_handler.ts
+++ src/webhooks/webhook_handler.ts
@@ -11,13 +11,14 @@ import { MappingsObject } from "../models/connection/mapping/mappings_object"
 export class WebhookHandler {
 
     async handleWebhook(data: any) {
+        console.log('about to handle webhook')
         const event: string = data.event
         const eventObject: string = data.eventObject
 
         let connectionId: ObjectId
 
         try {
-            connectionId = new ObjectId(data.connection._id)
+            connectionId = new ObjectId(data.connection)
         } catch (err) {
             return
         }
@@ -27,7 +28,7 @@ export class WebhookHandler {
         const service: string = data.service
         const lastUpdated: number | string = data.timestamp
         if (event === "DELETED") {
-            //delete
+            //deletes are ignored for now
             return
         }
         const newObject = this._getObjectFromWebhook(service, eventObject, lastUpdated, data.newObject)
@@ -36,26 +37,26 @@ export class WebhookHandler {
         switch (event) {
             case "CREATED":
                 switch (eventObject) {
-                    case "ISSUE":
+                    case "issue":
                         await this._createIssue(connection, service, lastUpdated, newObject as ServiceObject)
                         break
-                    case "PROJECT":
+                    case "project":
                         await this._createProject(connection, service, lastUpdated, newObject as ServiceObject)
                         break
-                    case "WORKLOG":
+                    case "worklog":
                         await this._createTimeEntry(connection, service, lastUpdated, newObject as TimeEntry)
                         break
                 }
                 break
             case "UPDATED":
                 switch (eventObject) {
-                    case "ISSUE":
+                    case "issue":
                         await this._updateIssue(connection, service, lastUpdated, newObject as ServiceObject)
                         break
-                    case "PROJECT":
+                    case "project":
                         await this._updateProject(connection, service, lastUpdated, newObject as ServiceObject)
                         break
-                    case "WORKLOG":
+                    case "worklog":
                         await this._updateTimeEntry(connection, service, lastUpdated, newObject as TimeEntry)
                         break
                 }
@@ -66,33 +67,37 @@ export class WebhookHandler {
 
     //issues
     async _createIssue(connection: Connection, service: string, lastUpdated: number | string, newIssue: ServiceObject) {
-        const secondService = connection.firstService.name === service ? connection.secondService : connection.firstService
+        console.log('about to create Issue')
+        const notCallingService = connection.firstService.name === service ? connection.secondService : connection.firstService
         let secondServiceObject
-        if (!this._isTicket2Ticket) {
+        if (!this._isTicket2Ticket(connection)) {
             //create tag in second service
-            const syncedService = SyncedServiceCreator.create(connection.secondService)
-            //secondServiceObject = await syncedService.createServiceObject(newIssue.id, newIssue.name, newIssue.type)
-            secondServiceObject = new ServiceObject(1, 'ahoj', 'Issue')
+            const syncedService = SyncedServiceCreator.create(notCallingService)
+            try {
+                secondServiceObject = await syncedService.createServiceObject(newIssue.id, newIssue.name, newIssue.type)
+            } catch (err) {
+                return
+            }
         } else {
-            //check if it is a pair 
             secondServiceObject = null
         }
         if (!secondServiceObject) {
             return
         }
+        const secondServiceType = notCallingService.name === 'Toggl Track' ? 'tag' : 'issue'
         //create mapping in connection
         const mapping = new Mapping()
         mapping.primaryObjectId = newIssue.id
         mapping.primaryObjectType = newIssue.type
         mapping.name = newIssue.name
         const primaryMappingObject = new MappingsObject(newIssue.id, newIssue.name, service, newIssue.type)
-        const secondaryMappingObject = new MappingsObject(secondServiceObject.id, secondServiceObject.name, secondService.name, newIssue.type)
+        const secondaryMappingObject = new MappingsObject(secondServiceObject.id, secondServiceObject.name, notCallingService.name, secondServiceType)
         mapping.mappingsObjects.push(primaryMappingObject)
         mapping.mappingsObjects.push(secondaryMappingObject)
         //save mapping to DB
-        console.log(mapping)
-        //connection.mappings.push(mapping)
-        //await databaseService.updateConnectionMappings(connection)
+        //console.log(mapping)
+        connection.mappings.push(mapping)
+        await databaseService.updateConnectionMappings(connection)
     }
     async _updateIssue(connection: Connection, service: string, lastUpdated: number | string, newIssue: ServiceObject) { }
     //async _deleteIssue(connectionId: string, service: string, lastUpdated: number | string, newIssue: ServiceObject) { }
@@ -109,15 +114,15 @@ export class WebhookHandler {
 
     private _getObjectFromWebhook(service: string, objType: string, lastUpdated: number | string, obj: any) {
         if (service === 'Jira') {
-            if (objType === 'WORKLOG') {
+            if (objType === 'worklog') {
+                //TODO this will fail on project Id
                 return new JiraTimeEntry(obj.id, obj.projectId, obj.text, new Date(obj.start), new Date(obj.end), obj.durationInMilliseconds, new Date(lastUpdated))
             }
-            if (objType === 'ISSUE' || objType === 'PROJECT') {
-                return new ServiceObject(obj.id, obj.name, obj.type, obj.projectId)
+            if (objType === 'issue' || objType === 'project') {
+                return new ServiceObject(obj.id, obj.name, obj.type)
             }
         }
-
-
+        return null
     }
 
     private _isTicket2Ticket(connection: Connection): boolean {
