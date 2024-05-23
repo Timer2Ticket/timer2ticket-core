edb91062cb13de50fadf6ce06ee09f7af538ef32
Max Hejda <heyda.max@gmail.com>
2024-05-23 15:31:49 +0200

    Implemented optimalization to not run multiple config jobs when using task ID as prefix in TE description

--- src/models/synced_service/time_entry/redmine_time_entry.ts
+++ src/models/synced_service/time_entry/redmine_time_entry.ts
@@ -11,9 +11,10 @@ export class RedmineTimeEntry implements TimeEntry {
   activityId: number;
   lastUpdated: Date;
   originalEntry: any;
+  needsConfigJob: boolean;
 
   constructor(id: number, projectId: number, text: string, start: Date, end: Date,
-    duration: number, issueId: number | undefined, activityId: number, lastUpdated: Date, originalEntry: any) {
+    duration: number, issueId: number | undefined, activityId: number, lastUpdated: Date, originalEntry: any, needsConfigJob: boolean = false) {
     this.id = id;
     this.projectId = projectId;
     this.text = text;
@@ -24,6 +25,7 @@ export class RedmineTimeEntry implements TimeEntry {
     this.activityId = activityId;
     this.lastUpdated = lastUpdated;
     this.originalEntry = originalEntry;
+    this.needsConfigJob = needsConfigJob;
   }
 
   // durationInMilliseconds = (): number => this.end.getTime() - this.start.getTime();
--- src/models/synced_service/time_entry/toggl_time_entry.ts
+++ src/models/synced_service/time_entry/toggl_time_entry.ts
@@ -10,8 +10,9 @@ export class TogglTimeEntry implements TimeEntry {
   tags: string[];
   lastUpdated: Date;
   originalEntry: any;
+  needsConfigJob: boolean
 
-  constructor(id: number, projectId: number, text: string, start: Date, end: Date, duration: number, tags: string[], lastUpdated: Date, originalEntry: any) {
+  constructor(id: number, projectId: number, text: string, start: Date, end: Date, duration: number, tags: string[], lastUpdated: Date, originalEntry: any, needsConfigJob = false) {
     this.id = id;
     this.projectId = projectId;
     this.text = text;
@@ -21,6 +22,7 @@ export class TogglTimeEntry implements TimeEntry {
     this.tags = tags;
     this.lastUpdated = lastUpdated;
     this.originalEntry = originalEntry;
+    this.needsConfigJob = needsConfigJob;
   }
 
   // durationInMilliseconds = (): number => this.end.getTime() - this.start.getTime();
